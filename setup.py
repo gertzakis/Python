789391b8f72969484a95bf050b708c7a0c738aab
Gerasimos Tzakis <gertzakis@gmail.com>
2017-12-07 20:20:51 +0200

    setupPlayer function

--- setup.py
+++ setup.py
@@ -1,5 +1,16 @@
+import os
+
+
+def clearScreen():  
+    """
+    Simple function that clears the screen
+    """
+    os.system('cls' if os.name=='nt' else 'clear')
 
 def showTitle():
+    """
+    Just printing the title: "Stick It to the MAN" on cmd.
+    """
     print("----------------------------------------------------------------------------------------")
     print("  _____ _   _      _      _____ _     _          _   _            __  __          _   _ ")
     print(" / ____| | (_)    | |    |_   _| |   | |        | | | |          |  \/  |   /\   | \ | |")
@@ -8,6 +19,25 @@ def showTitle():
     print(" ____) | |_| | (__|   <   _| |_| |_  | || (_) | | |_| | | |  __/ | |  | |/ ____ \| |\  |")
     print("|_____/ \__|_|\___|_|\_\ |_____|\__|  \__\___/   \__|_| |_|\___| |_|  |_/_/    \_\_| \_|")
     print("----------------------------------------------------------------------------------------")
+    print("                                                                              by GTzakis")
+    
+def setupPlayer():
+    """
+    Setting up Player.
+    Asking name of the player.
+    Setting up Health/Mana points(100/100).
+    """
+    # global name, Health Points, Mana Points, Rebel Points
+    global player_name
+    global player_health_points 
+    global player_mana_points
+    global player_rebel_points
+    # Our variable "name" is used to store our name, captured by keyboard input.
+    player_name = input("Who the hell do you think you are? \n")
+    # randint is a great way of adding some variety to your players statistics.        
+    player_health_points = 100
+    player_mana_points = 100
 
-
+clearScreen()
 showTitle()
+setupPlayer()
