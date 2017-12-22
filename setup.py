d6856fd2ef97b164fb844e9296f4d9d6ecc9a316
Gerasimos Tzakis <gertzakis@gmail.com>
2017-12-22 22:00:06 +0200

    Implementing Player class and some more setters

--- setup.py
+++ setup.py
@@ -3,11 +3,12 @@ import os
 #
 # Implementing functions for clear the terminal, and showing the Title.
 #
-def clearScreen():  
+def clearScreen():
     """
     Simple function that clears the screen
     """
-    os.system('cls' if os.name=='nt' else 'clear')
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def showTitle():
     """
@@ -23,15 +24,16 @@ def showTitle():
     print("----------------------------------------------------------------------------------------")
     print("                                                                              by GTzakis")
 
+#
 # Clear terminal and starting the game.
 clearScreen()
 showTitle()
 
 #
-# Implementing Class for basic characters.
+# Implementing basic Class for Characters.
 #
 class Character(object):
-    """Basic class for characters in the game, including the player"""
+    """Basic class for all the Characters in the game."""
 
     def __init__(self):
         """
@@ -41,52 +43,65 @@ class Character(object):
                 self.health (int, health points)
                 self.rage (int, rage points)
                 self.society (int, society points)
-
         """
         self.name = ''
         self.health = 100
-        self.rage = 0
-        self.society = 0
+        self.rage_points = 0
+        self.society_points = 0
     #
     # Implementing Getters.
     #
     def get_name(self):
         """Method for safely access the name"""
         return self.name
-    
+
     def get_health(self):
         """Method for safely get the health points"""
         return self.health
 
-    def get_rage(self):
-        """Method for safely get rage points""" 
-        return self.rage
+    def get_rage_points(self):
+        """Method for safely get rage points"""
+        return self.rage_points
 
-    def get_society(self):
+    def get_society_points(self):
         """Method for getting society points"""
-        return self.society
+        return self.society_points    
+    #
+    # Implementing needed setters.
+    #
+    def set_name(self, _character_name):
+        """Method for safely setting character name"""
+        self.name = _character_name
+    
+    def set_rage_points(self, _rage_points):
+        """Method for safely setting Rage Points"""
+        self.rage_points = _rage_points
+    
+    def set_society_points(self, _society_points):
+        """Method for safely setting Society(xp) Points"""
+        self.society_points = _society_points
+
+
+#
+# Implementing Player class, for the User's Player
+#
+class Player(Character):
+    """User's player class, inherits from Character class."""
+
+    def __init__(self, _username):
+        """
+            Initializes a Player object.
+            Attributes:
+                every attribute from Character-class objects.
+                setting name with the User's PlayerName.
+        """
+        Character.__init__(self)
+        self.set_name(_username)
+        
 
 
 #
 # This code will be deleted.
 #
-def setupPlayer():
-    """
-    Setting up Player.
-    Asking name of the player.
-    Setting up Health/Mana points(100/100).
-    """
-    # global name, Health Points, Mana Points, Rebel Points
-    global player_name
-    global player_health_points 
-    global player_mana_points
-    global player_rebel_points
-    # Our variable "name" is used to store our name, captured by keyboard input.
-    player_name = input("Who the hell do you think you are? \n")
-    # randint is a great way of adding some variety to your players statistics.        
-    player_health_points = 100
-    player_mana_points = 100
-
-
-user = Character()
-print(user.get_name(), user.get_health(), user.get_rage(), user.get_society())
+user = Player("MakisDaDog!")
+print(user.get_name(), user.get_health(), user.get_rage_points(), user.get_society_points())
